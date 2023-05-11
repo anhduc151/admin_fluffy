c0fbb56c6934212e379ae8110e5c2fe4d66f93ee
hao2dev <nghuuhao182001@gmail.com>
2023-05-12 04:10:46 +0700

    update view profile student

--- src/Pages/Student/index.js
+++ src/Pages/Student/index.js
@@ -157,11 +157,16 @@ function Student() {
                     <>
                       <div className='request_child'>
                         <h2 className='request_title1'>Schools: </h2>
-                        <p>{sch.name}</p>
-
+                        <h2 className='request_content'>{sch.name}</h2>
                       </div>
-                      <span className='request_span'>From: {dayFrom}/{monthFrom}/{yearFrom}</span>
-                      <span className='request_span'>To: {dayTo}/{monthTo}/{yearTo}</span>
+                      <div className='request_child'>
+                        <h2 className='request_title1'>From: </h2>
+                        <h2 className='request_content'>{dayFrom}/{monthFrom}/{yearFrom}</h2>
+                      </div>
+                      <div className='request_child'>
+                        <h2 className='request_title1'>To: </h2>
+                        <h2 className='request_content'>{dayTo}/{monthTo}/{yearTo}</h2>
+                      </div>
                     </>
                   )
                 })}
