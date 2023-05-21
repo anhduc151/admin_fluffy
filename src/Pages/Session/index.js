b22dad2bcb02b16ed9497f82673cbc9dadedadb5
hao2dev <nghuuhao182001@gmail.com>
2023-05-22 01:39:57 +0700

    feat sessions

--- src/Pages/Session/index.js
+++ src/Pages/Session/index.js
@@ -1,92 +1,217 @@
 import '../Session/session.css'
 import Sidebar from '../../components/SideBar/Sidebar';
 import Navbar from '../../components/Navbar';
-import { Table } from 'antd';
-import React, { useState } from "react";
+import { Button, Space, Table, Modal } from 'antd';
+import React, { useEffect, useState } from "react";
+import { gql, useMutation } from "@apollo/client";
+import client from "../../configGQL/index";
+import { ExclamationCircleFilled } from "@ant-design/icons";
+import { useDispatch } from 'react-redux';
+import { setError } from '../../Redux/feat/notificationSlice';
 
+
+const GET_BOOKED_SESSION = gql`
+  query getBookedSessions($queryParams: QueryFilterDto!){
+    getBookedSessions(queryParams: $queryParams){
+      items{
+        id
+        price
+        checkoutSessionId
+        status
+        data
+        isTransfer
+        course{
+          name
+        }
+        tutor{
+          lastName
+          firstName
+          email
+        }
+      }
+    }
+  }
+`
+const TRANSFER_TUTOR = gql`
+  mutation transferToTutorAfterCompletedCourse($bookedSessionId: String!){
+    transferToTutorAfterCompletedCourse(bookedSessionId: $bookedSessionId){
+      success
+      message
+    }
+  }
+`
 
 function Session() {
-    const columns = [
-        {
-          title: 'Name',
-          dataIndex: 'name',
-        },
-        {
-          title: 'Age',
-          dataIndex: 'age',
-        },
-        {
-          title: 'Address',
-          dataIndex: 'address',
-        },
-      ];
-      const data = [];
-      for (let i = 0; i < 5; i++) {
-        data.push({
-          key: i,
-          name: `Edward King ${i}`,
-          age: 32,
-          address: `London, Park Lane no. ${i}`,
+  const { confirm } = Modal;
+  const dispatch = useDispatch()
+  const [listBooked, setListBooked] = useState([])
+  // console.log("🚀 ~ file: index.js:35 ~ Session ~ listBooked:", listBooked)
+  useEffect(() => {
+    client
+      .query({
+        query: GET_BOOKED_SESSION,
+        variables: {
+          queryParams: {
+            page: 1,
+            limit: 99,
+          }
+        }
+      })
+      .then(respon => setListBooked(respon.data.getBookedSessions.items))
+      .catch(error => dispatch(setError({ message: error })))
+  }, [listBooked]);
+      
+
+  const [transferTutor] = useMutation(TRANSFER_TUTOR);
+  const handleCheckOut = (record) => {
+    // console.log(record);
+    // client
+    //   .mutate({
+    //     mutation: TRANSFER_TUTOR,
+    //     variables: {
+    //       bookedSessionId: record.id
+    //     }
+    //   })
+    //   .then(repons => dispatch(setError({ message: repons.data.message })))
+    //   .cacth(error => dispatch(setError({ message: error.message })))
+
+    const getData = async () => {
+      try {
+        const result = await transferTutor({
+          variables: {
+            bookedSessionId: record.id
+          },
         });
+        dispatch(setError({ message: result.data.transferToTutorAfterCompletedCourse.message }));
+      } catch (error) {
+        dispatch(setError({ message: error.message }));
       }
-    
-      const [selectedRowKeys, setSelectedRowKeys] = useState([]);
-      const onSelectChange = (newSelectedRowKeys) => {
-        setSelectedRowKeys(newSelectedRowKeys);
-      };
-      const rowSelection = {
-        selectedRowKeys,
-        onChange: onSelectChange,
-        selections: [
-          Table.SELECTION_ALL,
-          Table.SELECTION_INVERT,
-          Table.SELECTION_NONE,
-          {
-            key: 'odd',
-            text: 'Select Odd Row',
-            onSelect: (changeableRowKeys) => {
-              let newSelectedRowKeys = [];
-              newSelectedRowKeys = changeableRowKeys.filter((_, index) => {
-                if (index % 2 !== 0) {
-                  return false;
-                }
-                return true;
-              });
-              setSelectedRowKeys(newSelectedRowKeys);
-            },
-          },
-          {
-            key: 'even',
-            text: 'Select Even Row',
-            onSelect: (changeableRowKeys) => {
-              let newSelectedRowKeys = [];
-              newSelectedRowKeys = changeableRowKeys.filter((_, index) => {
-                if (index % 2 !== 0) {
-                  return true;
-                }
-                return false;
-              });
-              setSelectedRowKeys(newSelectedRowKeys);
-            },
-          },
-        ],
-      };
-    return (
-        <>
-            <div className="AppGlass">
-                <Navbar />
-                <div className='App_container'>
-                    <Sidebar />
-                    <div className='App_content'>
-                        <h1>Session</h1>
-                        <div className='payment_table'>
-                            <Table rowSelection={rowSelection} columns={columns} dataSource={data} />
-                        </div>
-                    </div>
-                </div>
+    };
+    getData();
+  }
+
+
+  const showPromiseConfirmTransfer = async (record) => {
+    if (record.status === 'COMPLETED') {
+      confirm({
+        title: `Do you want transfer to tutor ?`,
+        icon: <ExclamationCircleFilled />,
+        onOk: () => handleCheckOut(record),
+        onCancel() { },
+      });
+    } else
+      confirm({
+        title: `Course is not done yet!!`,
+        icon: <ExclamationCircleFilled />,
+        okButtonProps: { style: { display: 'none' } },
+        onCancel() { },
+      });
+  };
+  const columns = [
+    {
+      title: 'Tutor',
+      dataIndex: 'tutor',
+      key: 'tutor'
+    },
+    {
+      title: 'Course',
+      dataIndex: 'course',
+      key: 'course',
+      render: (text) => {
+        const truncatedText = `${text.substring(0, 40)}...`;
+        return <span>{truncatedText}</span>;
+      },
+    },
+    {
+      title: 'checkoutId',
+      dataIndex: 'checkoutId',
+      key: 'checkoutId',
+      render: (text) => {
+        const truncatedText = `${text.substring(0, 20)}...`;
+        return <span>{truncatedText}</span>;
+      },
+    },
+    {
+      title: 'Price',
+      dataIndex: 'price',
+      key: 'price',
+      render: (text) => {
+        const truncatedText = `${text} $`;
+        return <span>{truncatedText}</span>;
+      },
+    },
+    {
+      title: 'Status',
+      dataIndex: 'status',
+      key: 'status'
+    },
+    {
+      title: 'Action',
+      dataIndex: 'action',
+      key: 'action',
+      render: (text, record) => {
+        if (record.status === 'COMPLETED') {
+          if (text) {
+            return (
+              <Button
+                type='primary'
+                className='request_approve'
+                disabled={true}
+              >
+                Transferred
+              </Button>
+            );
+          } else {
+            return (
+              <Button
+                type='primary'
+                className='request_approve'
+                onClick={() => showPromiseConfirmTransfer(record)}
+              >
+                Transfer
+              </Button>
+            );
+          }
+        } else {
+          return (
+            <Button
+              type='primary'
+              className='request_approve'
+              disabled={true}
+            >
+              Transfer
+            </Button>
+          );
+        }
+      },
+    },
+  ];
+  const data = listBooked.map(data => ({
+    id: data.id,
+    tutor: data.tutor.firstName + " " + data.tutor.lastName,
+    course: data.course.name,
+    checkoutId: data.checkoutSessionId,
+    price: data.price,
+    status: data.status,
+    action: data.isTransfer,
+  }))
+
+  return (
+    <>
+      <div className="AppGlass">
+        <Navbar />
+        <div className='App_container'>
+          <Sidebar />
+          <div className='App_content'>
+            <h1>Session</h1>
+            <div className='payment_table'>
+              <Table columns={columns} dataSource={data} />
             </div>
-        </>
-    );
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Session;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,5 @@
 // import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import DashBoard from '../Pages/Dashboard';
-import Courses from '../Pages/Courses'
 import Payment from '../Pages/Payment'
 import Student from '../Pages/Student'
 import Tutor from '../Pages/Tutor'
@@ -11,7 +10,6 @@ import Session from '../Pages/Session'
 const publicRoutes = [
         { path: '/signin', component: SignIn},
         { path: '/', component: DashBoard },
-        { path: '/courses', component: Courses},
         { path: '/payment', component: Payment},
         { path: '/students', component: Student},
         { path: '/tutors', component: Tutor},
